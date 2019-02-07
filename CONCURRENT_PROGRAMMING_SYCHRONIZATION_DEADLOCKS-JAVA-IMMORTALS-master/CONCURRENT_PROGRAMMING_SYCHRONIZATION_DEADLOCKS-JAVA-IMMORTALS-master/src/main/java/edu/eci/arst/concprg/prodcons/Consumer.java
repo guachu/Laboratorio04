0edf33b4bf9b5bd0d38437c2335fd71d24936b60
juan felipe mora <43444251+juanmora98@users.noreply.github.com>
2019-02-07 18:29:59 -0500

    Add files via upload

---
+++ CONCURRENT_PROGRAMMING_SYCHRONIZATION_DEADLOCKS-JAVA-IMMORTALS-master/CONCURRENT_PROGRAMMING_SYCHRONIZATION_DEADLOCKS-JAVA-IMMORTALS-master/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Consumer extends Thread{
+    
+    private Queue<Integer> queue;
+    
+    
+    public Consumer(Queue<Integer> queue){
+        this.queue=queue;        
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+//
+//            if (queue.size() > 0) {
+//                int elem=queue.poll();
+//                System.out.println("Consumer consumes "+elem);                                
+//            }
+
+            try {
+                int elem = ((LinkedBlockingQueue<Integer>)queue).take();
+                System.out.println("Consumer consumes "+elem);
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
+}
---
+++ CONCURRENT_PROGRAMMING_SYCHRONIZATION_DEADLOCKS-JAVA-IMMORTALS-master/CONCURRENT_PROGRAMMING_SYCHRONIZATION_DEADLOCKS-JAVA-IMMORTALS-master/src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StartProduction {
+    
+    
+    public static void main(String[] args) {
+        
+        
+        
+        int limit = 2000;
+        Queue<Integer> queue=new LinkedBlockingQueue<>(limit);
+        new Producer(queue,limit).start();
+        
+        //let the producer create products for 5 seconds (stock).
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(StartProduction.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+        
+        new Consumer(queue).start();
+    }
+    
+
+}
